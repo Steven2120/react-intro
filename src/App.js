36cd3d0d7deffeeb8c820816f834d414e0057327
Steven Moyano <stevenmoyano1@gmail.com>
2022-06-11 19:01:13 -0500

    completed required tasks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <MyInfoComponent />
         </p>
         <a
           className="App-link"
@@ -21,5 +22,100 @@ function App() {
     </div>
   );
 }
+const MyInfoComponent = () => {
+  const [myName, setMyName] = useState();
+  const [myFavoriteColor, setMyFavoriteColor] = useState();
+  const [myFavoriteMovies, setMyFavoriteMovies] = useState([]);
+  const defaultInputColor = "red";
+
+  return (
+    <div>
+      <span style={{ backgroundColor: myFavoriteColor }}>
+        Hello, my name is {myName}
+      </span>
+      <br></br>
+      <input
+        style={{ backgroundColor: defaultInputColor }}
+        type="text"
+        onChange={(event) => {
+          const name = event.target.value;
+          setMyName(name);
+        }}
+      ></input>
+      <br></br>
+      <br></br>
+      <span style={{ backgroundColor: myFavoriteColor }}>
+        {" "}
+        My favorite color is: {myFavoriteColor}
+      </span>
+
+      <br></br>
+
+      <input
+        style={{ backgroundColor: defaultInputColor }}
+        type="text"
+        onChange={(event) => {
+          const color = event.target.value;
+          setMyFavoriteColor(color);
+        }}
+      ></input>
+
+      <br></br>
+      <br></br>
+
+      <span style={{ backgroundColor: myFavoriteColor }}>
+        My favorite movies are :<br></br>
+      </span>
+
+      <span>
+        <input
+          style={{ backgroundColor: defaultInputColor }}
+          type="text"
+          onChange={(event) => {
+            const movie = event.target.value;
+            setMyFavoriteMovies([
+              movie,
+              myFavoriteMovies[1],
+              myFavoriteMovies[2],
+            ]);
+          }}
+        ></input>
+        &nbsp; &nbsp;
+        <input
+          style={{ backgroundColor: defaultInputColor }}
+          type="text"
+          onChange={(event) => {
+            const movie = event.target.value;
+            setMyFavoriteMovies([
+              myFavoriteMovies[0],
+              movie,
+              myFavoriteMovies[2],
+            ]);
+          }}
+        ></input>
+        &nbsp; &nbsp;
+        <input
+          style={{ backgroundColor: defaultInputColor }}
+          type="text"
+          onChange={(event) => {
+            const movie = event.target.value;
+            setMyFavoriteMovies([
+              myFavoriteMovies[0],
+              myFavoriteMovies[1],
+              movie,
+            ]);
+          }}
+        ></input>
+      </span>
+
+      <p style={{ backgroundColor: myFavoriteColor }}>
+        {myFavoriteMovies.map((movie) => (
+          <li>{movie}</li>
+        ))}
+      </p>
+      <br></br>
+    </div>
+  );
+};
 
 export default App;
